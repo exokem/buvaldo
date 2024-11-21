ccf62ece6001ac1b3d619834b9324ae1a2faf466
Exokem <ex010@live.com>
2024-11-21 18:15:51 -0500

    Background covers

--- src/components/static/bgcover.js
+++ src/components/static/bgcover.js
@@ -1,5 +1,14 @@
+import Image from "next/image"
 
-export default function BackgroundCover() 
+export default function BackgroundCover({src, alt=""}) 
 {
-	
+	return (
+		<Image 
+			src={src}
+			alt={alt}
+			className="top-0 left-0 right-0 fixed z-[-1]"
+		>
+		
+		</Image>
+	)
 }
