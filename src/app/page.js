806983541e9a19281ffa509b0b19bc9f457c9b77
Exokem <ex010@live.com>
2024-11-21 18:19:45 -0500

    Homepage cover

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,17 @@ import Image from "next/image"
 import {Button} from "@nextui-org/button"
 import {ParallaxProvider} from 'react-scroll-parallax'
 import Home from '@page/home'
+import cover from './bryan-cover.jpg'
+
+import BackgroundCover from '@comp/static/bgcover'
+
 export default function Page() 
 {
-	return <div className="bg-red-500">a</div>
+	return (
+		<>
+			{/* <div className="bg-red-500 h-[100vh] opacity-50 z-10">a</div> */}
+			<BackgroundCover src={cover}/>
+		</>
+	)
 	// return <ParallaxProvider><Home/></ParallaxProvider>
 }
