33ee633cf4c94a2952d2850dc7b669c333ccf275
Exokem <ex010@live.com>
2025-03-07 17:27:53 -0500

    Port inset section

---
+++ src/components/layout/InsetSection.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export function InsetShadowOverlay({className=''})
+{
+	return (
+		<div className={`
+			absolute w-full h-full top-0 left-0
+			rounded-lg
+			pointer-events-none
+			${className}
+			[box-shadow:_inset_0_0px_4px_rgba(0,0,0,0.2)]
+		`}/>
+	)
+}
+
+export const insetSectionBackgroundClassName = `
+	bg-lgray-d bg-opacity-50
+`
+
+export const insetSectionClassName = `
+	rounded-lg
+	overflow-clip
+	relative
+`
+
+export default function InsetSection({children, className = ''})
+{
+	return (
+		<div className={`
+			${insetSectionClassName}
+			${insetSectionBackgroundClassName}
+			${className}
+		`}>
+			{children}
+			<InsetShadowOverlay/>
+		</div>
+	)
+}
