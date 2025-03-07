9640ce2fe68bf941fc107d5beca52206905f2f86
Exokem <ex010@live.com>
2025-03-07 18:44:07 -0500

    Film details in modal

--- src/components/display/FilmModal.js
+++ src/components/display/FilmModal.js
@@ -1,4 +1,12 @@
-import {Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@nextui-org/react"
+import {
+	Button,
+	Divider,
+	Modal,
+	ModalBody,
+	ModalContent,
+	ModalFooter,
+	ModalHeader
+} from "@nextui-org/react"
 import load from '@api/loader'
 import Asset from "@comp/dynamic/asset"
 import InsetSection from "@comp/layout/InsetSection"
@@ -6,6 +14,7 @@ import {SectionHeading} from "@comp/layout/Section"
 import {inter, passionOne} from "@font"
 import MaterialIcon from "@comp/icon"
 import icons from "@api/icons"
+import {useState} from "react";
 
 function FilmModalHeader({film, onClose})
 {
@@ -25,7 +34,7 @@ function FilmModalHeader({film, onClose})
 					self-center
 				`}
 			>
-				{film.title}
+				Preview
 			</h1>
 			<Button
 				onClick={onClose}
@@ -53,6 +62,8 @@ function FilmModalHeader({film, onClose})
 
 export default function FilmModal({film, disclosure})
 {
+	const [expandDetails, setExpandDetails] = useState(false)
+
 	return (
 		<Modal
 			isOpen={disclosure.isOpen}
@@ -60,11 +71,12 @@ export default function FilmModal({film, disclosure})
 			hideCloseButton={true}
 			isDismissable={true}
 			backdrop='blur'
+			className={`text-stone-900`}
 			classNames={{
 				// backdrop: "z-[200]",
 				wrapper: 'z-[100]',
 				base: `
-					min-w-[40vw] 
+					min-w-[70vw] 
 					bg-amber-400
 					border-amber-400
 					min-h-[calc(calc(9/16)*40vw)]
@@ -95,11 +107,67 @@ export default function FilmModal({film, disclosure})
 							<FilmModalHeader film={film} onClose={onClose} />
 						</ModalHeader>
 						<ModalBody>
-							<InsetSection className={`min-h-full aspect-[calc(16/9)] rounded-xl`}>
-								<Asset
-									asset={film.embed}
-									className={`w-full h-full`}
-								/>
+							<InsetSection className={`
+								min-h-full 
+								aspect-[calc(16/9)] 
+								rounded-xl
+								bg-stone-400
+								bg-opacity-100
+								relative
+								flex flex-row
+								gap-3
+								${inter.className}
+							`}>
+								<div className={`
+									flex flex-col
+									grow
+									p-3 gap-3
+									bg-stone-500
+								`}>
+									<Asset
+										asset={film.embed}
+										className={`
+										rounded-xl
+										drop-shadow-lg
+										aspect-[calc(16/9)] 
+										outline-none
+										${expandDetails ? `
+											h-[30%]
+										` : `
+											absolute
+											z-[100]
+											w-full h-full
+											top-0 left-0
+										`}
+									`}
+									/>
+
+								</div>
+								<div className={`
+									flex flex-col
+									grow
+									px-9
+									text-xl
+									text-stone-900
+								`}>
+									<div className={`flex flex-col gap-3 py-9`}>
+										<h1 className={`
+											font-black text-4xl
+										`}>
+											<em>{film.title}</em>
+										</h1>
+										<p className={`text-2xl`}>
+											<b>{film.genre}</b> {film.categories !== undefined && film.categories.length !== 0 ? ` | ${film.categories.join(', ')}`: ''}
+										</p>
+										<p className={`text-2xl`}>{film.date}</p>
+									</div>
+									<Divider className={`bg-black h-0.5 rounded-lg`} />
+
+									<p className={`text-xl pt-9`}>
+										{film.description}
+									</p>
+								</div>
+
 							</InsetSection>
 							<Button
 								className={`
@@ -109,20 +177,23 @@ export default function FilmModal({film, disclosure})
 									font-bold ${inter.className}  
 									bg-amber-400
 									drop-shadow-lg
+									!opacity-100
 									hover:!opacity-100
 									hover:bg-amber-300
+									z-[200]
 									pl-3
 									text-black font-bold text-lg
 								`}
+								onClick={() => setExpandDetails(!expandDetails)}
 							>
 								<MaterialIcon
-									path={icons.add}
+									path={expandDetails ? icons.hide : icons.show}
 									className={`
 										fill-black
 									`}
 									size={24}
 								/>
-								Show Details
+								{expandDetails ? 'Hide Details' : 'Show Details'}
 							</Button>
 						</ModalBody>
 					</>
