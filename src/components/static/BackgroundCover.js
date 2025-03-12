e190b4cb06abbb303107d0f1fc97a1ddf18a8350
xokem <ex010@live.com>
2025-03-12 12:42:35 -0400

    Fix cover sizing

--- src/components/static/BackgroundCover.js
+++ src/components/static/BackgroundCover.js
@@ -8,6 +8,11 @@ export default function BackgroundCover({src, alt=""})
 			alt={alt}
 			className={`
 				-translate-x-[calc(calc(100%-100vw)/2)]
+				h-[100vh] lg:h-auto 
+				w-[calc(1.5*100vh)] lg:w-auto
+				max-w-[100000px] lg:max-w-full
+				
+				!aspect-[3/2]
 				fixed z-[-10]
 				
 				top-[0]
